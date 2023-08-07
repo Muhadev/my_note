25c41ef606673f5e3d18076f739cf348ea9106df
Muhadev <muhammedfayemi@gmail.com>
2023-08-07 02:32:28 +0100

    task is added

---
+++ 3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int alphabet;
+    for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+    {
+        putchar(alphabet);
+    }
+    for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+    {
+        putchar(alphabet);
+    }
+    putchar('\n');
+    return (0);
+}
